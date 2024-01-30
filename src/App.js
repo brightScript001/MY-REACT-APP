aabf4d0fb5f6ab3da3bddd004e788b24b8a62ac5
Success Bright <bs3webdevelop@gmail.com>
2024-01-30 12:57:09 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function MyButton() {
+  return <button className="btn btn-primary">Click me!</button>;
+}
+
+export default function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My First React App</h1>
+      <MyButton />
+      <h2>Welcome, Nice to meet you</h2>
+      <MyButton />
     </div>
   );
 }
-
-export default App;
